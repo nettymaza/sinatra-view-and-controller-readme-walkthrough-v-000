5a016580102907e5a7a4a220f13a4e5923aac636
Annette Maza <nettymaza@gmail.com>
2017-10-04 22:26:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,10 +6,10 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    original_string = params["string"]
-    @reversed_string = orginal_string.reverse
-
-    erb :reversed
+  original_string = params["string"]
+  @reversed_string = original_string.reverse
+ 
+  erb :reversed
   end
 
   get '/friends' do
